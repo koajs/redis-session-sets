a3a5ddba584ddbc549834d18f48ecf85dc558796
jongleberry <me@jongleberry.com>
2016-03-01 13:37:15 -0800

    tests++

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const _request = require('supertest')
+const assert = require('assert')
 const redis = require('ioredis')
 const Koa = require('koa')
 
@@ -9,27 +10,43 @@ const Session = require('..')
 const client = redis.createClient()
 const keys = ['asdf', '1234']
 
-describe('Koa Redis Session Sets', () => {
+before(() => client.flushall())
+
+describe('context.session', () => {
   describe('should get and set the session', () => {
     const app = Koa()
     app.keys = keys
     app.use(Session(app, {
+      maxAge: '1 month',
+      prefix: 'asdf',
+      references: {
+        string: {},
+      },
       client,
     }))
     app.use(function * () {
       switch (this.method) {
-        case 'GET': {
-          const session = this.body = yield this.session.get()
-          this.assert.equal(session.number, 1)
-          this.assert.equal(session.string, 'string')
-          break
-        }
         case 'POST': {
           yield this.session.set({
             number: 1,
             string: 'string',
           })
           this.status = 204
+          break
+        }
+        case 'GET': {
+          const session = yield this.session.get()
+          this.assert.equal(session.number, 1)
+          this.assert.equal(session.string, 'string')
+
+          const key = this.session.getKey()
+          const obj = yield client.hgetall(key)
+          this.assert(obj)
+          this.assert.equal(obj.number, 1)
+          this.assert.equal(obj.string, 'string')
+
+          this.status = 204
+          break
         }
       }
     })
@@ -44,7 +61,61 @@ describe('Koa Redis Session Sets', () => {
     it('.session.get()', done => {
       agent
       .get('/')
-      .expect(200, done)
+      .expect(204, done)
+    })
+  })
+
+  describe('should unset the session', () => {
+    const app = Koa()
+    app.keys = keys
+    app.use(Session(app, {
+      client,
+    }))
+    app.use(function * () {
+      switch (this.method) {
+        case 'POST': {
+          yield this.session.set({
+            number: 1,
+            string: 'string',
+          })
+          this.status = 204
+          break
+        }
+        case 'DELETE': {
+          yield this.session.unset([
+            'number',
+            'string',
+          ])
+          this.status = 204
+          break
+        }
+        case 'GET': {
+          const session = yield this.session.get()
+          this.assert(!session.number)
+          this.assert(!session.string)
+          this.status = 204
+          break
+        }
+      }
+    })
+    const agent = _request.agent(app.listen())
+
+    it('.session.set()', done => {
+      agent
+      .post('/')
+      .expect(204, done)
+    })
+
+    it('.session.unset()', done => {
+      agent
+      .delete('/')
+      .expect(204, done)
+    })
+
+    it('.session.get()', done => {
+      agent
+      .get('/')
+      .expect(204, done)
     })
   })
 
@@ -155,6 +226,28 @@ describe('Koa Redis Session Sets', () => {
       agent
       .get('/')
       .expect(204, done)
+    })
+  })
+})
+
+describe('session', () => {
+  it('.getReferenceKey()', () => {
+    const app = Koa()
+    app.keys = keys
+    const session = Session(app, {
+      // same options as first test
+      maxAge: '1 month',
+      prefix: 'asdf',
+      references: {
+        string: {},
+      },
+      client,
+    })
+    app.use(session)
+
+    const key = session.getReferenceKey('string', 'string')
+    return client.smembers(key).then(results => {
+      assert(results.length)
     })
   })
 })
